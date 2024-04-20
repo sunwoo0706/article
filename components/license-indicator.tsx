2e8728d713328ba9033b0b861fb294ee82caa4d1
sunwoo0706 <sunwoo0706@icloud.com>
2024-04-20 18:27:13 +0900

    feat: update license indicator size

--- components/license-indicator.tsx
+++ components/license-indicator.tsx
@@ -3,12 +3,12 @@ import { Icons } from "./icons"
 export function LicenseIndicator() {
   return (
     <div className="-translate-1/2 fixed bottom-2 left-1/2">
-      <p className="text-sm font-normal text-[#696565]">
+      <p className="text-xs font-normal text-[#696565]">
         write by{" "}
         <a
           href="https://sunwoo.xyz"
           target="_blank"
-          className="inline-block size-6 align-middle"
+          className="inline-block size-5 align-middle"
           rel="noreferrer"
         >
           <Icons.sign fill="#696565" />
