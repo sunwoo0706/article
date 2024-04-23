68479ac4c2510d4e25c0f95f957a16563e49f523
sunwoo0706 <sunwoo0706@icloud.com>
2024-04-23 19:04:28 +0900

    feat: udpate anchor range

--- components/license-indicator.tsx
+++ components/license-indicator.tsx
@@ -3,17 +3,14 @@ import { Icons } from "./icons"
 export function LicenseIndicator() {
   return (
     <div className="-translate-1/2 fixed bottom-2 left-1/2">
-      <p className="text-xs font-normal text-[#696565]">
-        write by{" "}
-        <a
-          href="https://sunwoo.xyz"
-          target="_blank"
-          className="inline-block size-5 align-middle"
-          rel="noreferrer"
-        >
-          <Icons.sign fill="#696565" />
-        </a>
-      </p>
+      <a href="https://sunwoo.xyz" target="_blank" rel="noreferrer">
+        <p className="text-xs font-normal text-[#696565]">
+          write by{" "}
+          <span className="inline-block size-5 align-middle">
+            <Icons.sign fill="#696565" />
+          </span>
+        </p>
+      </a>
     </div>
   )
 }
