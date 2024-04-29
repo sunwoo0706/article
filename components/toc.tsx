0e0403c8d19749ade404209b7acb3279813b580f
sunwoo0706 <sunwoo0706@icloud.com>
2024-04-29 17:03:56 +0900

    fix: invalid tailwindcss class

--- components/toc.tsx
+++ components/toc.tsx
@@ -7,7 +7,7 @@ export function Toc() {
     <div className="fixed top-0.5 -translate-x-44 translate-y-32">
       <Link
         href="/"
-        className="font-regular flex items-center gap-2.5 text-sm text-[#696565]"
+        className="flex items-center gap-2.5 text-sm font-normal text-[#696565]"
       >
         <Icons.cornerLeft className="size-4" />홈
       </Link>
