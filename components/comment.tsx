6aca9c6286674c6b49b8ce2fa3f0417255e47912
sunwoo0706 <sunwoo0706@icloud.com>
2024-05-01 17:05:46 +0900

    feat: update comment sheet name to comment

--- components/comment.tsx
+++ components/comment.tsx
@@ -1,12 +1,10 @@
 import { Giscus } from "./giscus"
-import { Button } from "./ui/button"
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 
-export function CommectSheet() {
+export function Comment() {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="outline">Open</Button>
       </SheetTrigger>
       <SheetContent>
         <div className="hide-scroll h-full overflow-y-scroll py-4">
