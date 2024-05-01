a06c88510e69f00ba75c8b48b7a4827c2936fdff
sunwoo0706 <sunwoo0706@icloud.com>
2024-05-02 03:38:44 +0900

    fix: next-mdx-remote Error [ERR_REQUIRE_ESM]: require() of ES Module issue

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   reactStrictMode: true,
+  // https://github.com/hashicorp/next-mdx-remote/issues/381
+  transpilePackages: ["next-mdx-remote"],
   experimental: {
     appDir: true,
   },
