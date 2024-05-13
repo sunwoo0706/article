7b4b9548a159e71b28858b27b2e9bc2fd545cb61
sunwoo0706 <sunwoo0706@icloud.com>
2024-05-13 18:27:38 +0900

    feat: add article render animation

--- components/article-card-list.tsx
+++ components/article-card-list.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+import { motion } from "framer-motion"
+
 import { ArticleType } from "@/types/article"
 
 import { ArticleCard } from "./ui/article-card"
@@ -9,8 +13,18 @@ export interface IArticleProps {
 export function ArticleCardList({ articles }: IArticleProps) {
   return (
     <div className="flex flex-col gap-1">
-      {articles.map((article) => (
-        <ArticleCard article={article} />
+      {articles.map((article, i) => (
+        <motion.div
+          initial={{ y: 60, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            duration: 0.8 + i * 0.2,
+            bounce: 0,
+          }}
+        >
+          <ArticleCard article={article} />
+        </motion.div>
       ))}
     </div>
   )
