54a44798a28d840dbeba0f6e426dbe27a9f990b3
sunwoo0706 <sunwoo0706@icloud.com>
2024-05-16 16:32:11 +0900

    feat: add article card animation delay

--- components/article-card-list.tsx
+++ components/article-card-list.tsx
@@ -1,7 +1,3 @@
-"use client"
-
-import { motion } from "framer-motion"
-
 import { ArticleType } from "@/types/article"
 
 import { ArticleCard } from "./ui/article-card"
@@ -14,17 +10,7 @@ export function ArticleCardList({ articles }: IArticleProps) {
   return (
     <div className="flex flex-col gap-1">
       {articles.map((article, i) => (
-        <motion.div
-          initial={{ y: 60, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            type: "spring",
-            duration: 0.8 + i * 0.2,
-            bounce: 0,
-          }}
-        >
-          <ArticleCard article={article} />
-        </motion.div>
+        <ArticleCard article={article} index={i} />
       ))}
     </div>
   )
--- components/ui/article-card.tsx
+++ components/ui/article-card.tsx
@@ -1,41 +1,67 @@
+"use client"
+
+import { useEffect, useState } from "react"
 import { cx } from "class-variance-authority"
+import { motion } from "framer-motion"
 import { Link } from "next-view-transitions"
 
 import { ArticleType } from "@/types/article"
 
 interface ArticleCardProps extends React.HTMLAttributes<HTMLElement> {
   article: ArticleType
+  index: number
 }
 
 export function ArticleCard({
   className,
   article,
+  index,
   ...props
 }: ArticleCardProps) {
+  const [shouldAnimate, setShouldAnimate] = useState<boolean>(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShouldAnimate(true)
+    }, 100 * index)
+
+    return () => clearTimeout(timeout)
+  }, [index])
+
   return (
-    <Link href={`/${article.slug}`}>
-      <section
-        className={cx(
-          "flex w-full justify-start items-center rounded p-2 px-3 bg-transparent hover:bg-[#F7F7F7] transition-colors cursor-pointer",
-          className
-        )}
-        {...props}
-      >
-        <time
-          className="w-24 text-sm font-normal text-[#AEAEB2]"
-          dateTime={article.date}
+    <motion.div
+      initial={{ y: 60, opacity: 0 }}
+      animate={shouldAnimate && { y: 0, opacity: 1 }}
+      transition={{
+        type: "spring",
+        duration: 1.8,
+        bounce: 0,
+      }}
+    >
+      <Link href={`/${article.slug}`}>
+        <section
+          className={cx(
+            "flex w-full justify-start items-center rounded p-2 px-3 bg-transparent hover:bg-[#F7F7F7] transition-colors cursor-pointer",
+            className
+          )}
+          {...props}
         >
-          {article.date}
-        </time>
-        <p className="flex items-center gap-2 text-[0.9375rem] font-medium text-[#484848]">
-          {article.title}
-          {article.summary !== "" ? (
-            <span className="hidden text-sm font-medium text-[#AEAEB2] md:inline-block">
-              {article.summary}
-            </span>
-          ) : null}
-        </p>
-      </section>
-    </Link>
+          <time
+            className="w-24 text-sm font-normal text-[#AEAEB2]"
+            dateTime={article.date}
+          >
+            {article.date}
+          </time>
+          <p className="flex items-center gap-2 text-[0.9375rem] font-medium text-[#484848]">
+            {article.title}
+            {article.summary !== "" ? (
+              <span className="hidden text-sm font-medium text-[#AEAEB2] md:inline-block">
+                {article.summary}
+              </span>
+            ) : null}
+          </p>
+        </section>
+      </Link>
+    </motion.div>
   )
 }
