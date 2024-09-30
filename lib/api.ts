33620d972221727c29b27a97b9fe2662712a93fc
sunwoo0706 <sunwoo0706@icloud.com>
2024-09-30 09:14:11 +0900

    feat: update search logic

--- lib/api.ts
+++ lib/api.ts
@@ -79,7 +79,7 @@ export const getSearchedAllArticles = async (keywords: string) => {
     keywords
       .split(" ")
       .some((keyword) =>
-        hangulIncludes(`${article.title} ${article.summary}`, keyword)
+        hangulIncludes("".concat(article.title, article.summary), keyword)
       )
   )
 }
